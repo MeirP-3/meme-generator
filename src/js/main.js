8c9fd77101f1ca0b37218c773ae6cffb6c0ba5e0
Tamiross <tamir.mbox@gmail.com>
2017-06-06 14:31:32 +0300

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,12 @@ function filter() {
 
 }
 
+function showSearchBox() {
+    var box = document.querySelector('.search-container');
+    if (box.classList.contains('hidden')) {
+        box.classList.remove('hidden'); 
+    } else {
+        box.classList.add('hidden');
+    }
+    
+}   
