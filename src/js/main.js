3c750e821a638912f4efffb1d322b2c7b1c6d35b
Meir <pechtalt@gmail.com>
2017-06-06 17:04:30 +0300

    stop ask for massages! the work is in progress!

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,27 @@
 'use strict'
 
-var gSelectedImgUrl;
+// the first element in txts is the top text, the last is the bottom text
+var gState = {
+    selectedImgId: 0,
+    txts: [
+        {
+            content: '',
+            posX: 40,
+            posY: 40,
+            fontFamily: 'sans-serif',
+            fontSize: '30px',
+            color: 'black'
+        },
+        {
+            content: '',
+            posX: 40,
+            posY: 300,
+            fontFamily: 'sans-serif',
+            fontSize: '30px',
+            color: 'red'
+        }
+    ]
+};
 
 function createTagCloud() {
 
@@ -10,3 +31,16 @@ function filter() {
 
 }
 
+// update top text in model on any change
+$('.top > textarea').keyup(function () {
+    var content = $(this).val();
+    gState.txts[0].content = content;
+    drawCanvas();
+});
+
+// update bottom text in model on any change
+$('.bottom > textarea').keyup(function () {
+    var content = $(this).val();
+    gState.txts[1].content = content;
+    drawCanvas();
+});
--- src/js/render.js
+++ src/js/render.js
@@ -1,5 +1,8 @@
 'use strict'
 
+var MAX_WIDTH = 300;
+
+
 function renderImgs() {
 
 }
@@ -8,26 +11,37 @@ function renderTagCloud() {
 
 }
 
-function renderCanvas() {
-    renderImg();
-}
+function drawCanvas() {
 
-function renderImg() {
-
-    var imgUrl = gSelectedImgUrl;
     var context = $('canvas')[0].getContext('2d');
+
     var img = new Image();
+
+    var imgUrl = gImgsMap[gState.selectedImgId].url.lg;
 
     img.src = imgUrl;
 
     $(img).on('load', function () {
 
-        var width = $(img)[0].width;
-        var height = $(img)[0].height;
-        $('canvas').css('width', width);
-        $('canvas').css('height', height);
+        // handle dimensions
 
-        context.drawImage(img, 0, 0);
+        var width = Math.min(MAX_WIDTH, img.width);
+        var height = img.height;
+
+        $('canvas')[0].width = width;
+        $('canvas')[0].height = height;
+
+        context.drawImage(img, 0, 0, width, height);
+
+        // set default position of bottom text according to image size!
+        gState.txts[gState.txts.length - 1].posY = height - 10;
+
+        gState.txts.forEach(function (txt, i) {
+            var content = gState.txts[i].content;
+            context.font = '700 ' + gState.txts[i].fontSize + ' ' + gState.txts[i].fontFamily;
+            context.strokeStyle = gState.txts[i].color;
+            context.strokeText(content, gState.txts[i].posX, gState.txts[i].posY);
+        });
     });
 }
 
