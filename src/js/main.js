bba08e2104326d30d28088046dbad953d29a26c2
Tamiross <tamir.mbox@gmail.com>
2017-06-07 08:54:30 +0300

    fixing menu settings

--- src/js/main.js
+++ src/js/main.js
@@ -29,15 +29,21 @@ function filter() {
 
 }
 
-function showSearchBox() {
-    var box = document.querySelector('.search-container input');
-    if (box.classList.contains('hidden')) {
-        box.classList.remove('hidden'); 
+function toggleSearchBox() {
+    var elSearchBox = document.querySelector('.search-container input');
+    var elNavBar = document.querySelector('.navigation');
+    if (elSearchBox.classList.contains('hidden')) {
+        elSearchBox.classList.remove('hidden');
+        elNavBar.classList.add('hidden');
+
     } else {
-        box.classList.add('hidden');
+        elSearchBox.classList.add('hidden');
+        elNavBar.classList.remove('hidden');
     }
     
 }   
+
+
 // update top text in model on any change
 $('.top textarea').keyup(function () {
     var content = $(this).val();
